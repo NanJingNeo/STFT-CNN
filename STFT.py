85ccb451d8a98c374b76dabd438cbaeb2b0d984e
bupt-dyi <35458876+NanJingNeo@users.noreply.github.com>
2021-09-07 15:18:10 +0800

    Update STFT.py

--- STFT.py
+++ STFT.py
@@ -10,27 +10,31 @@ import scipy.io as sio
 from scipy.signal import stft
 import matplotlib.pyplot as plt
 
+#归一化函数
 def normalize(data):
     range = np.max(data)-np.min(data)
     return (data-np.min(data)) / range
 
 # 窗函数
-window = 'hann'
+window = 'hamming'
+# window = 'hann'
 # frame长度
 n = 26
-overlap=0.8*26
-
-Data = sio.loadmat('./begin/wcc_fall3.mat')["picS"];  #330*360 
-
+overlap=0.9*26
+# for i in range (1,31):
+# directory='./begin/wcc_fall'+str(i)+'.mat'
+# savedir='./begin/wcc_fall'+str(i)+'.png'
+directory='./begin/wcc_fall30.mat'
+# savedir='./begin/wcc_fall'+str(i)+'.png'
+Data = sio.loadmat(directory)["picS"];  #330*360 
 Data=normalize(Data).flatten()
 Data=Data.transpose()
 
-
 # STFT
 f, t, Z = stft(Data, fs=1.0, window=window, nperseg=n, noverlap = overlap )
+#列的分辨率为n/2+1
 # 求幅值
 Z = np.abs(Z)
 # # # 如下图所示
 plt.pcolormesh(t, f, Z, vmin = 0, vmax = Z.mean()*10)
-
-# plt.colorbar()
+    # plt.savefig(savedir, dpi=200)
