a805728d9b9b7dcef5cb939304fb0c9bb7a912fd
bupt-dyi <35458876+NanJingNeo@users.noreply.github.com>
2021-09-07 17:01:19 +0800

    Update STFT.py

--- STFT.py
+++ STFT.py
@@ -9,6 +9,8 @@ import numpy as np
 import scipy.io as sio
 from scipy.signal import stft
 import matplotlib.pyplot as plt
+import cv2
+
 
 #归一化函数
 def normalize(data):
@@ -21,20 +23,51 @@ window = 'hamming'
 # frame长度
 n = 26
 overlap=0.9*26
-# for i in range (1,31):
-# directory='./begin/wcc_fall'+str(i)+'.mat'
-# savedir='./begin/wcc_fall'+str(i)+'.png'
-directory='./begin/wcc_fall30.mat'
-# savedir='./begin/wcc_fall'+str(i)+'.png'
-Data = sio.loadmat(directory)["picS"];  #330*360 
-Data=normalize(Data).flatten()
-Data=Data.transpose()
+for i in range (1,31):
+    directory='./begin/wcc_fall'+str(i)+'.mat'
+    savedir='./begin/wcc_fall'+str(i)+'.png'
+    #    directory='./begin/wcc_fall30.mat'
+    # savedir='./begin/wcc_fall'+str(i)+'.png'
+    Data = sio.loadmat(directory)["picS"];  #330*360 
+    Data=normalize(Data).flatten()
+    Data=Data.transpose()
+    
+    # STFT
+    f, t, Z = stft(Data, fs=1.0, window=window, nperseg=n, noverlap = overlap )
+    #列的分辨率为n/2+1
+    # 求幅值
+    Z = np.abs(Z)
+    
+    #去除白边
+    # plt.axis('off')
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    plt.margins(0,0)
+    plt.pcolormesh(t, f, Z, vmin = 0, vmax = Z.mean())
+    plt.savefig(savedir, pad_inches = 0)
+    print(i)
 
-# STFT
-f, t, Z = stft(Data, fs=1.0, window=window, nperseg=n, noverlap = overlap )
-#列的分辨率为n/2+1
-# 求幅值
-Z = np.abs(Z)
-# # # 如下图所示
-plt.pcolormesh(t, f, Z, vmin = 0, vmax = Z.mean()*10)
-    # plt.savefig(savedir, dpi=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
